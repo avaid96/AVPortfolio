7a35bfcb29ec7d2555f737faf662bb96324bf966
[Avi Vaid] <[avaid1996@gmail.com]>
2015-04-07 18:45:47 -0500

    static navbar

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// $(document).ready(function() {
+//   console.log("ready!");
+// });
 $(document).ready(function() {
-  console.log("ready!");
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 550) {
+      $('#navbar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 550) {
+      $('#navbar').removeClass('navbar-fixed');
+    }
+  });
 });
