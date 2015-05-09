2b99eca3cf0b1eb46bf09139b3f4e1c3c2f408c9
[Avi Vaid] <[avaid1996@gmail.com]>
2015-05-09 15:43:34 -0500

    interest boxes

--- script.js
+++ script.js
@@ -22,3 +22,143 @@ $(document).ready(function() {
       return false;
   }); 
 });
+
+// codeslider
+$(document).ready(function(){
+    
+        var intervalTimer = null;
+    
+        $('#codeslider').hover(
+    
+            function () {
+                if($("a.active").is('.mercury')){
+                    $("#descriptionls").fadeIn("1000");
+                }
+                
+                if (intervalTimer !== null) {
+                    clearInterval(intervalTimer);
+                    intervalTimer = null;
+                }
+                
+                var quotes = [
+                    "C++",
+                    "Java",
+                    "Python",
+                    "HTML",
+                    "CSS",
+                    "JQuery",
+                    "Git",
+                    "C#",
+                 ];
+                    
+                var i = 0;
+                function update() {
+                     $("#codeslider").html(quotes[i]);
+                     i = (i + 1) % quotes.length;
+                }    
+                update();
+                intervalTimer = setInterval(update, 1000);
+            });
+    });
+
+// econslider
+$(document).ready(function(){
+    
+        var intervalTimer = null;
+    
+        $('#econslider').hover(
+    
+            function () {
+                if($("a.active").is('.mercury')){
+                    $("#descriptionls").fadeIn("1000");
+                }
+                
+                if (intervalTimer !== null) {
+                    clearInterval(intervalTimer);
+                    intervalTimer = null;
+                }
+                
+                var quotes = [
+                    "Intermediate Microeconomics",
+                    "Intro Macroeconomics",
+                    "Game theory",
+                    "Auctions",
+                    "Probability and statistics",
+                 ];
+                    
+                var i = 0;
+                function update() {
+                     $("#econslider").html(quotes[i]);
+                     i = (i + 1) % quotes.length;
+                }    
+                update();
+                intervalTimer = setInterval(update, 1000);
+            });
+    });
+
+// mathslider
+$(document).ready(function(){
+    
+        var intervalTimer = null;
+    
+        $('#mathslider').hover(
+    
+            function () {
+                if($("a.active").is('.mercury')){
+                    $("#descriptionls").fadeIn("1000");
+                }
+                
+                if (intervalTimer !== null) {
+                    clearInterval(intervalTimer);
+                    intervalTimer = null;
+                }
+                
+                var quotes = [
+                    "Linear algebra",
+                    "Multivariate calculus",
+                    "Probability and scotastic process",
+                    "Proofs",
+                 ];
+                    
+                var i = 0;
+                function update() {
+                     $("#mathslider").html(quotes[i]);
+                     i = (i + 1) % quotes.length;
+                }    
+                update();
+                intervalTimer = setInterval(update, 1000);
+            });
+    });
+
+// finslider
+$(document).ready(function(){
+    
+        var intervalTimer = null;
+    
+        $('#finslider').hover(
+    
+            function () {
+                if($("a.active").is('.mercury')){
+                    $("#descriptionls").fadeIn("1000");
+                }
+                
+                if (intervalTimer !== null) {
+                    clearInterval(intervalTimer);
+                    intervalTimer = null;
+                }
+                
+                var quotes = [
+                    "Equity research",
+                    "Bonds",
+                    "Governing finance",
+                 ];
+                    
+                var i = 0;
+                function update() {
+                     $("#finslider").html(quotes[i]);
+                     i = (i + 1) % quotes.length;
+                }    
+                update();
+                intervalTimer = setInterval(update, 1000);
+            });
+    });
